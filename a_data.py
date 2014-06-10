246696fbd648895ad35db9033f9add1407692fa8
vishaljain3991 <j.vishal3991@gmail.com>
2014-06-10 10:56:33 +0200

    Change to find location of dates file

--- a_data.py
+++ a_data.py
@@ -1,18 +1,13 @@
 import os
 import re, nltk, psycopg2 
-import dates, a_name
-from a_name import analysts
-from dates import converter
 
-conn = psycopg2.connect(database="finance", user="finance", password="iof2014", host="127.0.0.1", port="5432")
-
-fo = open("/home/finance/r_fil_loc.txt", "wb+")
+fo = open("/home/finance/reports2sql/r_fil_date.txt", "wb+")
 root = '/home/finance/data'
 #print os.walk(root, topdown=False)
 for path, subdirs, files in os.walk(root, topdown=False):
     for name in files:
         w=os.path.join(path, name)
-        if((re.search(r'^.*ratings.txt$', w))):
+        if((re.search(r'^.*dates$', w))):
         	print w
         	fo.write(w+" ")
         #print path[21:]
